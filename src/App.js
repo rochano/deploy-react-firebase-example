13e1328e170757970bf9b835e77770c84173568b
SIFDEV09 <SIFDEV09>
2017-11-01 11:27:02 +0700

    7

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Rochano</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.555556
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
